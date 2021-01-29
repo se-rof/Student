30b449a5a344a3f0b0719f1cc8101fb742a7337c
joshua1 <fortesra@sheridancollege.ca>
2021-01-29 11:55:15 -0500

    Added private variable address and getter and setter

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,10 +1,23 @@
 package student;
 
 public class Student {
+    private String name;
+    private String address;
+    
+    public String getName() {
+        return name;
+    }
 
-    public static void main(String[] args) {
-        
-
+    public void setName(String name) {
+        this.name = name;
     }
     
+    public String getAddress(String address) {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
