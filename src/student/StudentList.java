83d711c2b44d5d8e6cdf0410434709b6d996c790
joshua1 <fortesra@sheridancollege.ca>
2021-01-29 11:15:19 -0500

    Changed array size to 5

--- src/student/StudentList.java
+++ src/student/StudentList.java
@@ -11,7 +11,8 @@ package student;
  */
 public class StudentList {
     public static void main(String[] args){
-        Student [] studentList = new Student[2];
+        
+        Student [] studentList = new Student[3];
 
         Student s1 = new Student();
         s1.setName("Joshua");
