f148ad7cb7735a49201c504ff180ab05e29084d6
joshua1 <fortesra@sheridancollege.ca>
2021-02-05 09:59:10 -0500

    Added comments

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,5 +1,11 @@
 package student;
 
+/**for pull
+ * this is commend added
+ * This comment was added on 5th FEB at 9:18 to revise week 2 concepts
+ * This comment was added FROM github on 5th FEB 9:41 AM to check fetch and merge
+ */
+
 public class Student {
     private String name;
     private String address;
